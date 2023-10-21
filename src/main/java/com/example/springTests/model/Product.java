98d697cabe9375517eea5e79f7ac801a893cdf30
polsssha <Nusty11102003@gmail.com>
2023-10-21 13:48:17 +0500

    first commit

--- src/main/java/com/example/springTests/model/Product.java
+++ src/main/java/com/example/springTests/model/Product.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
